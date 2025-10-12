52cf4f7435e8194a5c84ec0e2ee9b02e3f992c29
VanHuyen193 <vanhuyen190304@gmail.com>
2025-10-12 10:03:34 +0700

    Product Variation

--- carts/views.py
+++ carts/views.py
@@ -12,6 +12,11 @@ def _cart_id(request):
     return cart
 
 def add_cart(request, product_id):
+    color = request.GET.get('color')
+    size = request.GET.get('size')
+    return HttpResponse(str(color) + ' ' + str(size))
+    exit()
+
     product = Product.objects.get(id=product_id) 
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart_id present in the session
