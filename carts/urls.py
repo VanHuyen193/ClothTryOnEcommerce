a921709462c0d68ef293b2da9f72259787a72f3c
VanHuyen193 <vanhuyen190304@gmail.com>
2025-10-12 22:23:01 +0700

    Delete cart item

--- carts/urls.py
+++ carts/urls.py
@@ -4,6 +4,6 @@ from . import views as view
 urlpatterns = [
     path('', view.cart, name='cart'),
     path('add/<int:product_id>/', view.add_cart, name='add_cart'),
-    path('remove_cart/<int:product_id>/', view.remove_cart, name='remove_cart'),
-    path('remove_cart_item/<int:product_id>/', view.remove_cart_item, name='remove_cart_item'),
+    path('remove_cart/<int:product_id>/<int:cart_item_id>/', view.remove_cart, name='remove_cart'),
+    path('remove_cart_item/<int:product_id>/<int:cart_item_id>/', view.remove_cart_item, name='remove_cart_item'),
 ]
--- carts/views.py
+++ carts/views.py
@@ -12,65 +12,87 @@ def _cart_id(request):
     return cart
 
 def add_cart(request, product_id):
-    product = Product.objects.get(id=product_id) 
+    product = Product.objects.get(id=product_id)
     product_variation = []
+
     if request.method == 'POST':
-        for item in request.POST:
-            key = item
-            value = request.POST[key]
+        # Lọc bỏ csrf token và các trường không phải variation
+        for key, value in request.POST.items():
+            if key == 'csrfmiddlewaretoken':
+                continue
             try:
-                variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+                variation = Variation.objects.get(
+                    product=product,
+                    variation_category__iexact=key,
+                    variation_value__iexact=value
+                )
                 product_variation.append(variation)
-            except:
+            except Variation.DoesNotExist:
                 pass
 
-    
+    # Tạo hoặc lấy cart
     try:
-        cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart_id present in the session
+        cart = Cart.objects.get(cart_id=_cart_id(request))
     except Cart.DoesNotExist:
-        cart = Cart.objects.create(
-            cart_id = _cart_id(request)
-        )
+        cart = Cart.objects.create(cart_id=_cart_id(request))
     cart.save()
 
-    try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
-        if len(product_variation) > 0:
-            cart_item.variations.clear()
-            for item in product_variation:
-                cart_item.variations.add(item)
+    # Lấy tất cả cart items cho product này trong cart
+    cart_items = CartItem.objects.filter(product=product, cart=cart)
 
-        cart_item.quantity += 1 # increase the cart item quantity
-        cart_item.save()
-    except CartItem.DoesNotExist:
-        cart_item = CartItem.objects.create(
-            product = product,
-            quantity = 1,
-            cart = cart,
-        )
-        if len(product_variation) > 0:
-            cart_item.variations.clear()
-            for item in product_variation:
-                cart_item.variations.add(item)
-        cart_item.save()
-    return redirect('cart')
+    # Chuẩn hóa product_variation thành tuple id đã sort để so sánh ổn định
+    product_var_ids = tuple(sorted([v.id for v in product_variation])) if product_variation else ()
 
+    if cart_items.exists():
+        ex_var_list = []   # danh sách tuple ids
+        id_list = []
 
-def remove_cart(request, product_id):
-    cart = Cart.objects.get(cart_id=_cart_id(request))
-    product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart)
-    if cart_item.quantity > 1:
-        cart_item.quantity -= 1
-        cart_item.save()
+        for cart_item in cart_items:
+            existing_var_ids = tuple(sorted([v.id for v in cart_item.variations.all()]))
+            ex_var_list.append(existing_var_ids)
+            id_list.append(cart_item.id)
+
+        # Nếu tồn tại item có cùng variation -> tăng quantity
+        if product_var_ids in ex_var_list:
+            index = ex_var_list.index(product_var_ids)
+            item_id = id_list[index]
+            existing_item = CartItem.objects.get(product=product, id=item_id)
+            existing_item.quantity += 1
+            existing_item.save()
+        else:
+            # Nếu chưa có -> tạo item mới và gán variation
+            new_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
+            if product_variation:
+                new_item.variations.add(*product_variation)
+            new_item.save()
     else:
-        cart_item.delete()
+        # Chưa có item nào -> tạo mới
+        new_cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
+        if product_variation:
+            new_cart_item.variations.add(*product_variation)
+        new_cart_item.save()
+
     return redirect('cart')
 
-def remove_cart_item(request, product_id):
+
+def remove_cart(request, product_id, cart_item_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart)
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except:
+        pass
+    return redirect('cart')
+
+def remove_cart_item(request, product_id, cart_item_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
     cart_item.delete()
     return redirect('cart')
 
