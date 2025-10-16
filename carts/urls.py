d9f0adab28d88e6ed735c27f1cce73c1c8bf90ea
VanHuyen193 <vanhuyen190304@gmail.com>
2025-10-16 22:25:06 +0700

    Checkout

--- carts/urls.py
+++ carts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add/<int:product_id>/', view.add_cart, name='add_cart'),
     path('remove_cart/<int:product_id>/<int:cart_item_id>/', view.remove_cart, name='remove_cart'),
     path('remove_cart_item/<int:product_id>/<int:cart_item_id>/', view.remove_cart_item, name='remove_cart_item'),
+    path('checkout/', view.checkout, name='checkout'),
 ]
