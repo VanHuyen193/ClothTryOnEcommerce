299ad7328b495fc0559b2ebc7925bcd23f7c40cd
VanHuyen193 <vanhuyen190304@gmail.com>
2025-11-21 12:12:39 +0700

    Change navbar, add product, filter product

--- store/views.py
+++ store/views.py
@@ -22,31 +22,44 @@ from .models import Product
 # Create your views here.
 def store(request, category_slug=None):
     categories = None
-    products = None
 
-    if category_slug != None:
+    # All products
+    if category_slug is not None:
         categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(
-            category=categories, is_available=True
-        )
-        paginator = Paginator(products, 3)
-        page = request.GET.get("page")
-        paged_products = paginator.get_page(page)
-        product_count = products.count()
+        products = Product.objects.filter(category=categories, is_available=True)
     else:
-        products = (
-            Product.objects.all().filter(is_available=True).order_by("id")
-        )
-        paginator = Paginator(products, 3)
-        page = request.GET.get("page")
-        paged_products = paginator.get_page(page)
-        product_count = products.count()
+        products = Product.objects.filter(is_available=True).order_by("id")
+
+    # Size
+    size = request.GET.get("size")
+    if size:
+        products = products.filter(
+            variation__variation_category="size",
+            variation__variation_value__iexact=size,
+            variation__is_active=True
+        ).distinct()
+
+    # Price
+    min_price = request.GET.get("min_price")
+    max_price = request.GET.get("max_price")
+
+    if min_price:
+        products = products.filter(price__gte=min_price)
+    if max_price:
+        products = products.filter(price__lte=max_price)
+
+    # Pagination
+    paginator = Paginator(products, 6)
+    page = request.GET.get("page")
+    paged_products = paginator.get_page(page)
+    product_count = products.count()
 
     context = {
         "products": paged_products,
         "product_count": product_count,
     }
     return render(request, "store/store.html", context)
+
 
 
 def product_detail(request, category_slug, product_slug):
